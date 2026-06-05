073ea8c12c105aa89d6bd208555d57d38bb2814d
Emmanuel Fotso <brigittembouda@gmail.com>
2026-06-04 17:49:54 -0700

    heyy

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,4 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-const link = document.createElement("link");
-link.rel = "icon";
-link.type = "image/png";
-link.href = "/assets/dgc-logo.png";
-document.head.appendChild(link);
-
-createRoot(document.getElementById("root")!).render(<App />);  
+createRoot(document.getElementById("root")!).render(<App />);
